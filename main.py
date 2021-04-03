2f2a791d424dc5b43a7f9fecf646f03df8947688
Abraham Magaña Hernández <abrahamm9986@gmail.com>
2021-04-03 01:27:50 -0600

    Second Commit

--- main.py
+++ main.py
@@ -66,26 +66,173 @@ def login():
 
 # This Function "Registers" the Product Input, here it's where the database is missing for now
 def register():
-    global mainWindow,IDTextBox,delete,productAdd
+    global mainWindow,IDTextBox,delete,productAdd,productTable,productPrice
     productType=IDTextBox.get()
-    if productType!="":
+    productType=productType.lower()
+    productType=productType.title()
+    if productType in ["Roles","Capuchino","Moka","Flat White","Latte","Chai","Dirty Chai","Chocolate","Golden Milk", "Taro",
+    "Cajeta","Cacao","Ice Latte","Panque","Brownie","Americano G","Soda Italiana S"]:
         if delete==False:
             pass
         else:
             clear(productAdd)
         productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
         productAdd.pack()
+        productPrice=35
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Frape Chocolate","Frape Café","Frape Cafe","Frape Cajeta","Frape Vainilla","Frape Oreo","Frape Taro","FrapeMazapan",
+    "Frape Moka","Cold Brew","Soda Italiana","Tisana","Dirty Chai G"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=40
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Doble","Cortado","Cortado Doble","Refrescos","Leche Santa Clara","Galletas Chispas","Lungo"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=20
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Malteada Chocolate","Malteada Vainilla","Malteada Fresa","Brownie Con Nieve","Brownie Nieve"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=55
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Espreso","Galletas Ketto"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=15
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Americano","Lungo G"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=25
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Te Snapple","Mini Panque"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=18
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Panini Jamon","Panini Jamón","Panini Jamón Pavo","Panini Jamon Pavo","Café 1/4","Cafe 1/4"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=65
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Panini Selva Negra"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=85
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Te Frio","Té Frio","Té","Te","Capuchino M","Moka M","Flat White M","Latte M","Chai M","Chocolate M"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=30
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Adiccion","Chocoqueso","Pastel Zanahoria","Muss Guayaba","Muss Frutos",
+    "Muss limon","Strudell","Corazon De Leon","Gansio Ketto","Pinguino Ketto","Chescake Brownie",
+    "Chescake fresa","Beso De Chocolate","Crunch De Menta","Domo Mazapan","Trufa","Tarta De Frutas"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=60
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Golden Milk G","Frape Especial"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=45
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
+        delete=True
+    elif productType in ["Café 1kg","Cafe 1kg"]:
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" Agregado!',font=("Calibri",10),fg="green3")
+        productAdd.pack()
+        productPrice=265
+        totalPrice=totalPrice+productPrice
+        productTable.insert("",0,text=productType,values=(f"${productPrice}"))
         delete=True
     else:
-        pass
+        if delete==False:
+            pass
+        else:
+            clear(productAdd)
+        productAdd=Label(mainWindow,text=f'¡Producto "{productType}" NO EXISTE!',font=("Calibri",10),fg="red2")
+        productAdd.pack()
+        delete=True
 
 # This Function it's my main window, not the root one, that's called "window"    
 def mainWindow():
-    global mainWindow,imageLogo,IDTextBox,delete
+    global mainWindow,imageLogo,IDTextBox,delete,productTable
     delete=False
     window.destroy()
     mainWindow=Tk()
-    mainWindow.resizable(0,0)
+    mainWindow.resizable(1,1)
     mainWindow.iconbitmap("titleIcon.ico")
     mainWindow.geometry("430x470")
     mainWindow.title("Café DiFiore - Administrador")
@@ -93,9 +240,9 @@ def mainWindow():
     imageLogo=PhotoImage(file="logoImage.gif")
     topLabel=Label(image=imageLogo)
     topLabel.pack()
-    productTable=ttk.Treeview(height=10,columns=2)
-    productTable.heading("#0",text="Nombre - ID",anchor=CENTER)
-    productTable.heading("#1",text="Precio - $",anchor=CENTER)
+    productTable=ttk.Treeview(mainWindow,columns=("#1",))
+    productTable.heading("#0",text="     Nombre",anchor=W)
+    productTable.heading("#1",text="     Precio",anchor=W)
     productTable.pack()
 
     titleLabel=Label(mainWindow, text="Ingrese el Tipo de Producto :", font=("Calibri",15))
